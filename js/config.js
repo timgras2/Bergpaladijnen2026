65d8426a1e71ab91097211fc9d1eb54868a28bb7
CodexSandboxOffline <CodexSandboxOffline@LT-10000268.superp.local>
2026-02-19 22:11:32 +0100

    Enable remote voting and load config on index page

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   remoteVoting: {
     // Set to true after configuring your Firebase Realtime Database URL.
-    enabled: false,
+    enabled: true,
     // Example: https://your-project-id-default-rtdb.europe-west1.firebasedatabase.app
     firebaseDatabaseUrl: 'https://bergpaladijnen-default-rtdb.europe-west1.firebasedatabase.app/',
     // How often clients refresh votes from remote storage.
