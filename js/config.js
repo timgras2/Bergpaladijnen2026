63aa5cf737fbfec46730797a4fd7224d5c9abdde
Tim Grasmaijer <tgra@superp.local>
2026-02-19 21:42:22 +0100

    Enable Firebase shared voting

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ window.APP_CONFIG = {
     // Set to true after configuring your Firebase Realtime Database URL.
     enabled: false,
     // Example: https://your-project-id-default-rtdb.europe-west1.firebasedatabase.app
-    firebaseDatabaseUrl: '',
+    firebaseDatabaseUrl: 'https://bergpaladijnen-default-rtdb.europe-west1.firebasedatabase.app/',
     // How often clients refresh votes from remote storage.
     pollIntervalMs: 8000,
   },
